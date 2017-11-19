cee243810e12f144afd66abe82efce57653630e4
said708 <said.thitah@usmba.ac.ma>
2017-11-19 20:18:12 +0000

    just a few updates

--- hibernate_tuto/src/org/mql/test/hibernate/App1.java
+++ hibernate_tuto/src/org/mql/test/hibernate/App1.java
@@ -1,13 +1,27 @@
 package org.mql.test.hibernate;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class App1 {
 
 	public App1() {
-		// TODO Auto-generated constructor stub
+		String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String password = "hbstudent";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("connecting to database :"+jdbcUrl);
+			 Connection con = DriverManager.getConnection(jdbcUrl , user , password);
+			System.out.println("Connection successful !!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new App1();
 
 	}
 
