2ed30d5da221d3b8e10c23a05639233114379ae6
said708 <said.thitah@usmba.ac.ma>
2017-11-19 17:16:29 +0000

    add hibernate library

---
+++ hibernate_tuto/src/org/mql/test/hibernate/App1.java
@@ -0,0 +1,14 @@
+package org.mql.test.hibernate;
+
+public class App1 {
+
+	public App1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
