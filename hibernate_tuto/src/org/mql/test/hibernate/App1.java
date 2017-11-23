1ca0a3d06f443a53016e380af3c06b8a1a57d7fa
said708 <said.thitah@usmba.ac.ma>
2017-11-23 16:01:34 +0000

    just  a few modification in hibernate DAO

--- hibernate_tuto/src/org/mql/test/hibernate/App1.java
+++ hibernate_tuto/src/org/mql/test/hibernate/App1.java
@@ -3,10 +3,20 @@ package org.mql.test.hibernate;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 
 public class App1 {
-
+	private static SessionFactory factory; 
 	public App1() {
+		exp02();
+	}
+
+	private void exp01() {
 		String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/hb_student_tracker?useSSL=false";
 		String user = "hbstudent";
 		String password = "hbstudent";
@@ -18,7 +28,42 @@ public class App1 {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
+	private void exp02() {
+		try {
+			Author said = new Author(1, "said", 1990);
+            // Create the SessionFactory from hibernate.cfg.xml
+        	Configuration configuration = new Configuration();
+        	configuration.configure("hibernate.cfg.xml");
+        	System.out.println("Hibernate Configuration loaded");
+        	
+        	ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        	System.out.println("Hibernate serviceRegistry created");
+        	
+        	SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        	Session session = sessionFactory.getCurrentSession();
+        	
+        	//start transaction
+    		session.beginTransaction();
+    		//Save the Model object
+    		session.save(said);
+    		//Commit transaction
+    		session.getTransaction().commit();
+    		System.out.println("Author ID="+said.getId());
+    		
+    		//terminate session factory, otherwise program won't end
+    		session.close();
+//            return sessionFactory;
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+//            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+	
 
 	public static void main(String[] args) {
 		new App1();
