843f3346c70e9f7f8b3e62b21cb8d4071ae21b5f
BrainyDev <yassine-trickz@hotmail.fr>
2018-01-11 16:52:56 +0000

    Just a simple correction from "Ensignant" to "Enseignant"

--- plateform/src/org/java/mql/dao/p1/EnseignantDao.java
+++ plateform/src/org/java/mql/dao/p1/EnseignantDao.java
@@ -3,12 +3,12 @@ package org.java.mql.dao.p1;
 import java.util.List;
 
 import org.java.mql.dao.DAOservice;
-import org.java.mql.models.p1.Ensignant;
+import org.java.mql.models.p1.Enseignant;
 
-public interface EnsignantDao extends DAOservice{
-	int addEnsignant(Ensignant ensignant);
-	Ensignant deleteEnsignant(long idEnsignant);
-	List<Ensignant> selectAllEnsignant();
-	Ensignant selectEnsignantById(long id);
-	int updateEnsignant(long idEnsignant , Ensignant ensignant);
+public interface EnseignantDao extends DAOservice{
+	int addEnsignant(Enseignant ensignant);
+	Enseignant deleteEnsignant(long idEnsignant);
+	List<Enseignant> selectAllEnsignant();
+	Enseignant selectEnsignantById(long id);
+	int updateEnsignant(long idEnsignant , Enseignant ensignant);
 }
